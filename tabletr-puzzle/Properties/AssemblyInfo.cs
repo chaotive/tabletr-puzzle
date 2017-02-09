93e2e5862d25dd82132b2e78e5b73205930cfc71
chaotive <leonardo.montenegro@chaotive.cl>
2017-02-09 14:49:21 -0300

    Fixing assembly info

--- tabletr-puzzle/Properties/AssemblyInfo.cs
+++ tabletr-puzzle/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("tabletr-puzzle-cl35")]
+[assembly: AssemblyTitle("tabletr-puzzle")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("tabletr-puzzle-cl35")]
+[assembly: AssemblyProduct("tabletr-puzzle")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
